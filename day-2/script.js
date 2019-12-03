9d76a87d269e33ac186f38ca5ab88827fd254fc4
Thuong Ho <thuong.t.ho@gmail.com>
2019-12-02 23:01:37 -0800

    aoc2 part 2

--- day-2/script.js
+++ day-2/script.js
@@ -2,54 +2,75 @@ const DATA = [1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,10,1,19,2,19,6,23,2,13,23,27,1,9
 
 const TEST = [1,9,10,3,2,3,11,0,99,30,40,50];
 
-checkOpCode = (pos, data) => {
-  const pos1 = data[pos];
-  const pos2 = data[data[pos + 1]];
-  const pos3 = data[data[pos + 2]];
-  if (pos1 === 1) {
+checkOpCode = (pointer, data) => {
+  const opcode = data[pointer];
+  const param2 = data[data[pointer + 1]];
+  const param3 = data[data[pointer + 2]];
+  if (opcode === 1) {
     // add the next 2 positions and store in 3rd position
-    return pos2 + pos3;
-  } else if (pos1 === 2) {
+    return param2 + param3;
+  } else if (opcode === 2) {
     // multiply the next 2 positions and store in 3rd position
-    return pos2 * pos3;
-  } else if (pos1 === 99) {
+    return param2 * param3;
+  } else if (opcode === 99) {
     // end program
-    console.log('restore finished.');
+    // console.log('restore finished.');
     return -1;
   } else {
     // error
-    console.log('found error: current number is ', pos1);
+    console.log('found error: current number is ', opcode);
     return -99;
   }
 };
 
-restoreState = (data) => {
-  const fixData = data;
+restoreState = (noun, verb, data) => {
+  const memory = data;
   // Restore to "1202 program alarm" state
   // replace position 1 with 12
-  fixData[1] = 12;
+  memory[1] = noun;
   // replace position 2 with 2
-  fixData[2] = 2;
+  memory[2] = verb;
 
-  let currPos = 0;
-  const length = fixData.length;
-  while (currPos < length) {
-    const code = checkOpCode(currPos, fixData);
+  let instPtr = 0;
+  const length = memory.length;
+  while (instPtr < length) {
+    const code = checkOpCode(instPtr, memory);
     if (code < 0) {
-      console.log('finish', fixData[0]);
-      return fixData;
+      // console.log('finish', memory[0]);
+      // return memory[0];
+      break;
     }
-    const updateIdx = fixData[currPos + 3];
-    fixData[updateIdx] = code;
-    currPos += 4;
-    // console.log('fixData', fixData);
+    const updateIdx = memory[instPtr + 3];
+    memory[updateIdx] = code;
+    instPtr += 4;
+    // console.log('memory', memory);
   }
   // what value is left at position 0
-  console.log('no 99');
-  return fixData[0];
+  // console.log('no 99');
+  const output = memory[0];
+  return output;
 }
 
+findNounVerb = (desiredOutput) => {
+  for (let noun = 0; noun <= 99; noun++) {
+    for (let verb = 99; verb >= 0; verb--) {
+      const cloneData = JSON.parse(JSON.stringify(DATA));
+      const output = restoreState(noun, verb, cloneData);
+      if (output === desiredOutput) return { noun, verb };
+    }
+  }
+  return 'answer not found.';
+};
+
 console.time('AOC2');
-restoreState(DATA);
+// part 1
 // restoreState(TEST);
+// const output = restoreState(12, 2, DATA);
+// console.log('output', output);
+
+// part 2
+// what pair of input produces 19690720
+const desiredOutput = 19690720;
+const inputs = findNounVerb(desiredOutput);
+console.log('inputs', inputs, 100 * inputs.noun + inputs.verb);
 console.timeEnd('AOC2');
